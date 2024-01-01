5efbc1ab3a5140cc5a987a5c94ebcbee12cba171
J <eyehorus.eth@protonmail.com>
2024-01-01 10:09:19 -0600

    fixes compile errors

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
-use num_bigint::{BigUint};
+use num_bigint::{BigUint, ToBigUint};
 use rand::Rng;
 use primal::is_prime;
+use num_traits::{One, ToPrimitive};
 
 fn generate_large_prime(bits: usize) -> BigUint {
     // generate random number with desired number of bits
     let mut rng = rand::thread_rng();
-    let candidate = rng.gen_biguint(bits as u64);
+    let mut candidate = rng.gen_range(0u64..(1u64 << bits)).to_biguint().unwrap();
 
     // use primality test to find next prime >= candidate
     let prime = loop {
-        if is_prime(&candidate.to_u64().unwrap()) {
+        if is_prime(candidate.to_u64().unwrap()) {
             break candidate.clone();
         }
-        candidate.clone_inc();
+        candidate += 1u32;
     };
     prime
 }
 
-fn dlogProof1(x: i32, g: i32, p: i32) -> [i32] {
+fn dlog_proof1(x: i32, g: i32, p: i32) -> i32 {
     // step one of proof
     let mut y = x.pow(g as u32);
     y = y % p;
@@ -26,32 +27,37 @@ fn dlogProof1(x: i32, g: i32, p: i32) -> [i32] {
 }
 
 fn main() {
-    let rand_generator = rand::thread_rng();
-    println!("Generator: {}", rand_generator);
-    let secret_id = rand::thread_rng();
-    println!("Secret ID: {}", secret_id);
+    let mut rng = rand::thread_rng();
+    
+    let rand_generator = rng.gen::<i32>();
+    println!("Generator: {:?}", rand_generator);
+    
+    let secret_id = rng.gen::<i32>();
+    println!("Secret ID: {:?}", secret_id);
+    
     let large_prime = generate_large_prime(4);
-    println!("Large Prime: {}", large_prime);
+    println!("Large Prime: {:?}", large_prime);
 
     // prover sends y = x^g(mod p)
     println!("Prover calculating y = rand_generator ^ secret_id");
-    let h = dlogProof1(rand_generator, secret_id, large_prime);
-    
+    let _h = dlog_proof1(rand_generator, secret_id, large_prime.clone().to_u32().unwrap() as i32);
+
     // verifier sends back random bit 
     println!("Verifier sending random bit...");
-    let mut verifier_random_bit = rand::thread_rng();
-    verifier_random_bit.gen_range(0..=1);
+    let verifier_random_bit = rng.gen_range(0..=1);
 
     // prover sends s = (rand_generator + verifier_random_bit * secret_id)(mod (large_prime - 1))
     println!("Prover sending s = (rand_generator + verifier_random_bit * secret_id)(mod (large_prime - 1))");
     let mut s = rand_generator + verifier_random_bit * secret_id;
-    s = s % (large_prime - 1);
+    s = s % (large_prime.clone() - BigUint::one()).to_u32().unwrap() as i32;
 
     println!("Verifier computing rand_generator ^ s(mod large_prime)");
-    let mut verifier_solution = rand_generator.pow(s);
-    verifier_solution = verifier_solution % large_prime;
+    let mut verifier_solution = rand_generator.to_biguint().unwrap().pow(s as u32) % &large_prime;
+    verifier_solution = verifier_solution % &large_prime;
     
-    println!("verifier_solution should equal h^verifer_random_bit(mod large_prime)");
-    let compared_solution = h.pow(verifier_random_bit) % large_prime;
+    println!("verifier_solution should equal h^verifier_random_bit(mod large_prime)");
+    let compared_solution = verifier_solution.modpow(&(verifier_random_bit as u32).into(), &large_prime);
     println!("verifier_solution: {}, compared_solution: {}", verifier_solution, compared_solution);
 }
+
+
